adb8897e3dca968fe31cf02604d170e902d310bf
Joshua Elder <aresx130@gmail.com>
2017-11-29 13:43:28 -0800

    Add query for getUserByEmail().

--- src/models/db/authentication.js
+++ src/models/db/authentication.js
@@ -7,6 +7,14 @@ const createUser = (user) => {
     })
 }
 
+const getUserByEmail = (email) => {
+  return db.oneOrNone('SELECT * FROM users WHERE email = $1', [email])
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
+
 module.exports = {
   createUser,
+  getUserByEmail,
 }
