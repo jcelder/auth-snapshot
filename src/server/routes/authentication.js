9f3be675256269717645173777262ac7b062b411
Joshua Elder <aresx130@gmail.com>
2017-11-29 13:28:23 -0800

    Add create user functionality to '/login' route.

--- src/server/routes/authentication.js
+++ src/server/routes/authentication.js
@@ -1,8 +1,17 @@
 const router = require('express').Router()
+const db = require('../../models/authentication')
 
 router.route('/signup')
   .get((req, res) => {
     res.render('authentication/signup')
+  })
+  .post((req, res) => {
+    const user = req.body
+    console.log(req.body)
+    db.create(user)
+      .then(() => {
+        res.redirect('/login')
+      })
   })
 
 router.route('/login')
