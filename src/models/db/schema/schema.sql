8d0b98201aab0ff2e999e86485afd00ec3b37904
Joshua Elder <aresx130@gmail.com>
2017-11-29 11:57:18 -0800

    Added users table.

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -5,3 +5,9 @@ CREATE TABLE contacts (
   first_name varchar(255) NOT NULL,
   last_name varchar(255) NOT NULL
 );
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
