ef1eba5367d61fb9658098fe7666544e3a43a06f
Joshua Elder <aresx130@gmail.com>
2017-11-29 15:39:11 -0800

    Add encryption to user password on '/signup' route.

--- src/server/routes/authentication.js
+++ src/server/routes/authentication.js
@@ -1,16 +1,41 @@
 const router = require('express').Router()
 const db = require('../../models/authentication')
+const bcrypt = require('bcrypt')
+
+/**
+ * Encrypts a password using bcrypt
+ * @param {string} plaintextPassword
+ * @returns {Promise} - Promise which resolves to a hashed password
+ */
+const encryptUserPassword = (plaintextPassword) => {
+  return bcrypt.genSalt(10)
+    .then((salt) => {
+      return bcrypt.hash(plaintextPassword, salt)
+    })
+    .then((hash) => {
+      console.log(hash)
+      return hash
+    })
+}
 
 router.route('/signup')
   .get((req, res) => {
     res.render('authentication/signup')
   })
   .post((req, res) => {
-    const user = req.body
+    const email = req.body.email
+    const plaintextPassword = req.body.password
 
-    db.create(user)
-      .then(() => {
-        res.redirect('/login')
+    encryptUserPassword(plaintextPassword)
+      .then((hash) => {
+        const newUser = {
+          email: email,
+          encrypted_password: hash,
+        }
+        db.create(newUser)
+          .then(() => {
+            res.redirect('/login')
+          })
       })
   })
 
