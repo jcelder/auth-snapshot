1dfd2d2d889322e9dc338a3328ec6228ed81de94
Joshua Elder <aresx130@gmail.com>
2017-11-29 13:27:14 -0800

    Added createUser() query.

--- src/models/db/authentication.js
+++ src/models/db/authentication.js
@@ -1,5 +1,12 @@
 const db = require('./db')
 
-const addUser = (user) = {
-  return db.one('INSERT INTO users VALUES()'
+const createUser = (user) => {
+  return db.none('INSERT INTO users (email, encrypted_password) VALUES (${email}, ${password})', user)
+    .catch((err) => {
+      console.log(err.message)
+    })
+}
+
+module.exports = {
+  createUser,
 }
