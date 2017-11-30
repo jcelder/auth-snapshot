0572725fa00ef050d99452c1cfa4d285b265fbc8
Josh <aresx130@gmail.com>
2017-11-30 10:29:06 -0800

    Add 'session' table for connect-pg-simple.

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -11,3 +11,11 @@ CREATE TABLE users (
   email VARCHAR(255) UNIQUE NOT NULL,
   encrypted_password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE "session" (
+  "sid" varchar NOT NULL COLLATE "default",
+	"sess" json NOT NULL,
+	"expire" timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
