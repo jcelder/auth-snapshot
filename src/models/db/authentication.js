3022a449ef17d261e6c87fca72460bc051ebb4b6
Josh <aresx130@gmail.com>
2017-11-30 13:25:32 -0800

    Chain query to add role to database by 'user.id'.

--- src/models/db/authentication.js
+++ src/models/db/authentication.js
@@ -1,7 +1,10 @@
 const db = require('./db')
 
 const createUser = (user) => {
-  return db.none('INSERT INTO users (email, encrypted_password) VALUES (${email}, ${encrypted_password})', user)
+  return db.one('INSERT INTO users (email, encrypted_password) VALUES (${email}, ${encrypted_password}) RETURNING id', user)
+    .then((user_id) => {
+      return db.none('INSERT INTO user_roles (user_id, roles_id) VALUES ($1, $2)', [user_id.id, 2])
+    })
     .catch((err) => {
       console.log(err.message)
     })
